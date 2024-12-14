6eb40d2a5ea90a92305aea2cb7f51bf15b9f810a
KirKhokhlov <khokhlov785@mail.ru>
2024-12-14 19:47:13 +0300

    Add auth api conn

--- react-frontend/src/components/client/AuthPage/AuthPage.js
+++ react-frontend/src/components/client/AuthPage/AuthPage.js
@@ -1,12 +1,13 @@
 import './AuthPage.css';
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../general/AuthContext/AuthContext';
 import Cookies from 'js-cookie';
 
 export default function AuthPage(props) {
     const location = useLocation();
-    console.log(location.pathname)
+    let navigate = useNavigate();
+
 
     const [login, setLogin] = useState("");
     const [validLogin, setValidLogin] = useState(true);
@@ -17,11 +18,12 @@ export default function AuthPage(props) {
     const [passwordAgain, setPasswordAgain] = useState("");
     const [validPassAgain, setPassAgain,] = useState(true);
 
-    const {setUserData} = useAuthContext();
+    const { setUserData } = useAuthContext();
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     let formContent;
+    let formBtn;
     let buttonText;
     let headerText;
 
@@ -42,10 +44,96 @@ export default function AuthPage(props) {
     }
 
     // -----------------------
-    const handleEnter = () => {
-        let newUserData = {role: login, name: login}
-        Cookies.set("userData", JSON.stringify(newUserData), {expires: 7, sameSite: 'strict'})
-        setUserData(newUserData);
+
+
+    const handleEnter = async () => {
+        const authData = { email: email, password: password }
+        const authDataStr = JSON.stringify(authData)
+        try {
+            let response = await fetch("http://127.0.0.1:8000/api/login", {
+                method: 'POST',
+                // mode: "no-cors",
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                    'Accept': 'application/json;charset=utf-8'
+                },
+                body: authDataStr
+            })
+            if (response.status === 200) {
+                let responseData = await response.json()
+                Cookies.set("userData", JSON.stringify(responseData), { expires: 7, sameSite: 'strict' })
+                setUserData(responseData);
+                alert("Вы успешно авторизованы!");
+                navigate("/");
+            } else {
+                let responseData = await response.json()
+                let messages = "";
+                if (responseData.errors) {
+                    let errors = responseData.errors
+                    if (errors.email) {
+                        messages = messages.concat(`\n- ${errors.email[0]}`)
+                    }
+                    if (errors.password) {
+                        messages = messages.concat(`\n- ${errors.password[0]}`)
+                    }
+                }
+                if (response.message) {
+                    messages = messages.concat(`\n${response.message}`)
+                }
+                alert(`Произошла ошибка при входе.${messages}`);
+            }
+        } catch (e) {
+            alert(`Не удалось авторизоваться. Попробуйте позже`);
+        }
+
+    }
+
+    const handleReg = async () => {
+        handleLoginBlur()
+        handlePassBlur()
+        handlePassAgainBlur()
+        handleEmailBlur()
+        if (!(validEmail && validLogin && validPassword && validPassAgain)) {
+            alert("Ошибка в введенных данных")
+            return;
+        }
+        const authData = { login: login, email: email, password: password, password_repeat: passwordAgain }
+        const authDataStr = JSON.stringify(authData)
+        try {
+            let response = await fetch("http://127.0.0.1:8000/api/registration", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                    'Accept': 'application/json;charset=utf-8'
+                },
+                body: authDataStr
+            })
+            if (response.status === 201) {
+                let responseData = await response.json()
+                Cookies.set("userData", JSON.stringify(responseData), { expires: 7, sameSite: 'strict' })
+                setUserData(responseData);
+                alert("Вы успешно зарегестрированы!");
+                navigate("/");
+            } else {
+                let responseData = await response.json()
+                let messages = "";
+                if (responseData.errors) {
+                    let errors = responseData.errors
+                    if (errors.email) {
+                        messages = messages.concat(`\n- ${errors.email[0]}`)
+                    }
+                    if (errors.password) {
+                        messages = messages.concat(`\n- ${errors.password[0]}`)
+                    }
+                }
+                if (response.message) {
+                    messages = messages.concat(`\n${response.message}`)
+                }
+                alert(`Произошла ошибка при регистрации.${messages}`);
+            }
+        } catch (e) {
+            alert(`Не удалось зарегестрировать. Попробуйте позже`);
+        }
     }
 
 
@@ -55,17 +143,23 @@ export default function AuthPage(props) {
         buttonText = "Войти";
 
         formContent = <>
-            <label>Логин</label>
+            <label>Почта</label>
             <input className={validLogin ? '' : 'invalid-input'}
                 title={validLogin ? '' : 'Логин должен иметь не менее 4-х символов'}
                 type='text' placeholder='Введите логин'
-                onChange={(value) => setLogin(value.target.value)}
+                onChange={(value) => setEmail(value.target.value)}
             ></input>
             <label>Пароль</label>
             <input className={validPassword ? '' : 'invalid-input'}
                 title={validPassword ? '' : 'Пароль должен содержать не менее 5-ти символов'}
                 onChange={(value) => setPassword(value.target.value)}
                 type='password' placeholder='Введите пароль'></input>
+        </>
+
+        formBtn = <>
+            <div className='button-block'>
+                <button className='auth-button' onClick={handleEnter}>{buttonText}</button>
+            </div>
         </>
     }
     else {
@@ -100,10 +194,12 @@ export default function AuthPage(props) {
                 onBlur={handlePassAgainBlur}
                 type='password' placeholder='Введите пароль'></input>
         </>
+        formBtn = <>
+            <div className='button-block'>
+                <button className='auth-button' onClick={handleReg}>{buttonText}</button>
+            </div>
+        </>
     }
-
-
-
 
     return (
         <div className='auth-back'>
@@ -115,9 +211,7 @@ export default function AuthPage(props) {
                         {formContent}
                     </form>
                 </div>
-                <div className='button-block'>
-                    <button className='auth-button' onClick={handleEnter}>{buttonText}</button>
-                </div>
+                {formBtn}
             </div>
 
         </div>
