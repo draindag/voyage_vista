55da91fbf24179d342c2fbd51d04fa02f69e63df
KirKhokhlov <khokhlov785@mail.ru>
2025-01-02 04:02:14 +0300

    fix mini profile

--- react-frontend/src/components/client/Navbar/Navbar.js
+++ react-frontend/src/components/client/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import './Navbar.css';
 import '../../../resources/constants.css';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ProfileManager from '../ProfileManager/ProfileManager';
 
 
@@ -14,9 +14,14 @@ import logo from '../../../resources/Navbar/Images/logo.svg';
 
 
 export default function Navbar() {
+    const countrySelect = useRef(null);
+
     const [scrolled, setScrolled] = useState(false);
     const [showProfileManager, setShowProfileManager] = useState(false);
     const [paddingTop, setPaddingTop] = useState(30);
+
+    const [positions, setPositions] = useState({ select: null, profile: null })
+
     const handleScroll = () => {
         const scrollTop = window.scrollY;
         if (scrollTop > 30) {
@@ -28,36 +33,56 @@ export default function Navbar() {
         }
     };
 
+    const handleResize = () => {
+        const profile = document.querySelector('.profile-button').getBoundingClientRect();
+        const select = document.querySelector('.country-button').getBoundingClientRect();
+        setPositions({ select: select, profile: profile });
+    }
+
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleResize);
+        handleResize();
         // Удаляем обработчик при размонтировании компонента
         return () => {
             window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
 
 
 
 
-    let navClasses = `${scrolled? 'scrolled': ''} navbar`
+    let navClasses = `${scrolled ? 'scrolled' : ''} navbar`
     return <>
-            <div className={navClasses} id="navbar" style={{ paddingTop: `${paddingTop}px` }}>
-                <div className="header-logo">
-                    <img src={logo} alt="" />
-                    <span style={{maxWidth: "188px", textWrap: "wrap"}}>Voyage vista</span>
-                </div>
-                <div className="header-navbar" id="header-navbar" style={{background: scrolled ? 'none' : 'var(--navbar-background)'}}>
-                    <div className="navbar-content">
-                        <a href="/"><img src={home} alt="" /><span>Главная</span></a>
-                        <a href="/countryes"><img src={country} alt="" /><span>Страны</span></a>
-                        <a href="/tourist"><img src={tourist} alt=""/><span>Туристам</span></a>
-                        <a href="/contacts"><img src={contacts} alt="" /><span>Контактная информация</span></a>
-                        <button onClick={() => setShowProfileManager(!showProfileManager)}><img src={personalAccount} alt="" /><span>Мой кабинет</span></button>
-                    </div>
+        <div className={navClasses} id="navbar" style={{ paddingTop: `${paddingTop}px` }}>
+            <div className="header-logo">
+                <img src={logo} alt="" />
+                <span style={{ maxWidth: "188px", textWrap: "wrap" }}>Voyage vista</span>
+            </div>
+            <div className="header-navbar" id="header-navbar" style={{ background: scrolled ? 'none' : 'var(--navbar-background)' }}>
+                <div className="navbar-content">
+                    <a href="/"><img src={home} alt="" /><span>Главная</span></a>
+                    {/* <a href="/countryes"><img src={country} alt="" /><span>Страны</span></a> */}
+                    <button id='country-button' className='country-button' onClick={() => countrySelect.current.click()}><img src={country} alt="" /><span>Страны</span></button>
+                    <select
+                        style={{ display: 'none' }}
+                        ref={countrySelect}
+                    >
+                        <option>asfasfasf</option>
+                    </select>
+                    <a href="/tourist"><img src={tourist} alt="" /><span>Туристам</span></a>
+                    <a href="/contacts"><img src={contacts} alt="" /><span>Контактная информация</span></a>
+                    <button id='profile-button' className='profile-button' onClick={() => setShowProfileManager(!showProfileManager)}><img src={personalAccount} alt="" /><span>Мой кабинет</span></button>
                 </div>
             </div>
-            {showProfileManager ? <ProfileManager onExit={() => {setShowProfileManager(!showProfileManager)}}/> : null}
+        </div>
+        {showProfileManager ? <ProfileManager onExit={() => { setShowProfileManager(!showProfileManager) }} style={{
+            position: 'absolute',
+            top: `${positions.profile?.bottom+50}px`,
+            left: `${positions.profile?.left}px`,
+        }} /> : null}
     </>
 }
 
