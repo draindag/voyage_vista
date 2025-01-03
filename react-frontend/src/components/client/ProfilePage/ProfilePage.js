8f00a0ea0fbf990d13279177f09c7ac3a98a473e
KirKhokhlov <khokhlov785@mail.ru>
2025-01-03 08:03:09 +0300

    profile change panel

--- react-frontend/src/components/client/ProfilePage/ProfilePage.js
+++ react-frontend/src/components/client/ProfilePage/ProfilePage.js
@@ -6,55 +6,26 @@ import TourSlider from './TourSlider';
 import { useNavigate, useParams } from 'react-router-dom';
 
 
-
-
 import '../ToursPage/ToursPage.css';
 import { fetchData, sendData } from '../../general/web_ops';
 import { deleteCookie } from '../../general/cookie_ops';
 import dayjs from 'dayjs'
 
 
-const tour = {
-
-    tour_image: '0d2b7241-fb36-4154-8fd8-729438486411.png',
-
-    tour_title: 'Приключение на Гавайях',
-
-    tour_start_date: '2025-01-10',
-
-    tour_end_date: '2025-01-20',
-
-    tour_description: 'Незабываемое путешествие на самый красивый остров.',
-
-    country: { country_name: 'США' },
-
-    tour_price: 1000,
-
-    price_with_discount: 800,
-
-}
-
-const toursData = [
-    tour, tour, tour, tour
-
-
-    // Добавьте дополнительные туры здесь
-
-];
-
-
-
-
-
 
 export default function ProfilePage() {
     const [state, setState] = useState({ user: null, favor: [{}], tours: [{}] });
-    const [tab, setTab] = useState(1)
+    const [tab, setTab] = useState(1);
+    const [changes, setChanges] = useState(null);
+
+    const [newProfileData, setNewProfileData] = useState({ new_login: '', password: '', email: '', new_password: '' })
+
 
     const prevUserDataRef = useRef();
 
     let navigate = useNavigate();
     console.log(state)
+    console.log(newProfileData)
     // console.log(tab)
 
 
@@ -65,15 +36,15 @@ export default function ProfilePage() {
         <div>
             <div className='profile-tour-card'>
                 <h1 className='profile-tour-card-h1'>{item.tour_title}</h1>
-    
-                <div style={{paddingLeft: "66px"}}>
+
+                <div style={{ paddingLeft: "66px" }}>
                     <div style={{ display: 'flex' }}>
                         <div>
                             <label className='slide-label'>Дата:</label>
-                            <input className='slide-input' 
-                            style={{ width: '244px', marginRight: '130px' }} 
-                            type='text'
-                            defaultValue={`${dayjs(item.tour_start_date).format("DD.MM.YY")}-${dayjs(item.tour_end_date).format("DD.MM.YY")}`}></input>
+                            <input className='slide-input'
+                                style={{ width: '244px', marginRight: '130px' }}
+                                type='text'
+                                defaultValue={`${dayjs(item.tour_start_date).format("DD.MM.YY")}-${dayjs(item.tour_end_date).format("DD.MM.YY")}`}></input>
                         </div>
                         <div>
                             <label className='slide-label'>Страна:</label>
@@ -89,21 +60,21 @@ export default function ProfilePage() {
                         <input className='slide-input' type='text' defaultValue={item?.category?.category_title}></input>
                     </div>
                     <div className='profile-tour-card-line-div'>
-                        <label className='slide-label'>Стоимость с <br/>учетом скидки:</label>
+                        <label className='slide-label'>Стоимость с <br />учетом скидки:</label>
                         <input className='slide-input' type='text' defaultValue={`${item.price_with_discount}руб`}></input>
                     </div>
                     {/* <div style={{display: 'flex', justifyContent: 'end'}}>
                         <label>Итого</label>
                     </div> */}
                 </div>
-    
-    
-    
-    
+
+
+
+
             </div>
         </div>
     ));
-    
+
     const smallTour = state.favor?.map((item, index) => (
         <div className='tour-card'>
             <div className='tour-card-image-container'>
@@ -123,7 +94,7 @@ export default function ProfilePage() {
             </div>
         </div>
     ));
-    
+
 
 
 
@@ -176,29 +147,83 @@ export default function ProfilePage() {
     let panelButtons;
     switch (tab) {
         case 1:
-            content = <>
-                <label className='profile-data-label'>Логин:</label>
-                <div>
-                    <input className='profile-data-input' type='text' defaultValue={state.user?.login}></input>
-                    <button style={{ display: 'inline-block' }}>Изменить</button>
-                </div>
-                <label className='profile-data-label'>Почта:</label>
-                <div>
-                    <input className='profile-data-input' type='text' defaultValue={state.user?.email}></input>
-                    <button style={{ display: 'inline-block' }}>Изменить</button>
-                </div>
-                <label className='profile-data-label'>Пароль:</label>
-                <div>
-                    <input className='profile-data-input' type='password' defaultValue='password'></input>
-                    <button style={{ display: 'inline-block' }}>Изменить</button>
-                </div>
-            </>;
+            switch (changes) {
+                case 1:
+                    name = 'ИЗМЕНЕНИЕ ЛОГИНА'
+                    content = <>
+                        <p className='profile-data-label'>Логин:</p>
+                        <input id='new-login' className='profile-data-input' type='text'
+                            // value={newProfileData.new_login}
+                            onChange={(value) => setNewProfileData({ ...newProfileData, new_login: value.target.value })}
+                        ></input>
+                        <p className='profile-data-label'>Пароль:</p>
+                        <input id='confirm-pass' className='profile-data-input' type='password'
+                            // value={newProfileData.password}
+                            onChange={(value) => setNewProfileData({ ...newProfileData, password: value.target.value })}
+                        ></input>
+                    </>
+
+                    break;
+                case 2:
+                    name = 'ИЗМЕНЕНИЕ ПОЧТЫ'
+                    content = <>
+                        <label className='profile-data-label'>Почта:</label>
+                        <input className='profile-data-input' type='text'
+                            // value={newProfileData.email}
+                            onChange={(value) => setNewProfileData({ ...newProfileData, email: value.target.value }) }
+                        ></input>
+                        <label className='profile-data-label'>Пароль:</label>
+                        <input className='profile-data-input' type='password'
+                            // value={newProfileData.password}
+                            onChange={(value) => setNewProfileData({ ...newProfileData, password: value.target.value }) }
+                        ></input>
+                    </>
+                    break;
+                case 3:
+                    name = 'ИЗМЕНЕНИЕ ПАРОЛЯ'
+                    content = <>
+                        <label className='profile-data-label'>Старый пароль:</label>
+                        <input className='profile-data-input' type='text' 
+                            // value={newProfileData.password}
+                            onChange={(value) => setNewProfileData({ ...newProfileData, password: value.target.value }) }
+                        ></input>
+                        <label className='profile-data-label'>Новый пароль:</label>
+                        <input className='profile-data-input' type='password'
+                            // value={newProfileData.new_password}
+                            onChange={(value) => setNewProfileData({ ...newProfileData, new_password: value.target.value }) }
+                        ></input>
+                        <button className='submit-new-profile-button'>Изменить</button>
+                    </>
+                    break;
+                default:
+                    name = 'ПРОФИЛЬ';
+                    content = <>
+                        <p className='profile-data-label'>Логин:</p>
+                        <div>
+                            <input className='profile-data-input' type='text' defaultValue={state.user?.login}></input>
+                            {/* <p className='profile-data-input profile-data-input-p' >{state.user?.login}</p> */}
+                            <button className='profile-data-button' style={{ display: 'inline-block' }} onClick={() => { setChanges(1) }}>Изменить</button>
+                        </div>
+                        <p className='profile-data-label'>Почта:</p>
+                        <div>
+                            <input className='profile-data-input' type='text' defaultValue={state.user?.email}></input>
+                            {/* <p className='profile-data-input profile-data-input-p' >{state.user?.email}</p> */}
+                            <button className='profile-data-button' style={{ display: 'inline-block' }} onClick={() => { setChanges(2) }}>Изменить</button>
+                        </div>
+                        <p className='profile-data-label'>Пароль:</p>
+                        <div>
+                            <input className='profile-data-input' type='password' defaultValue='password'></input>
+                            {/* <p className='profile-data-input profile-data-input-p' >***</p> */}
+                            <button className='profile-data-button' style={{ display: 'inline-block' }} onClick={() => { setChanges(3) }}>Изменить</button>
+                        </div>
+                    </>;
+                    break;
+            }
             panelButtons = <>
-                <button style={{ backgroundColor: '#5D7EA7' }} onClick={() => { setTab(1) }}>ПРОФИЛЬ</button>
+                <button style={{ backgroundColor: '#5D7EA7' }} onClick={() => { setTab(1); setChanges(null); }}>ПРОФИЛЬ</button>
                 <button onClick={() => { setTab(2) }}>ИЗБРАННОЕ</button>
                 <button onClick={() => { setTab(3) }}>МОИ ТУРЫ</button>
             </>;
-            name = 'ПРОФИЛЬ';
             break;
         case 2:
             panelButtons = <>
