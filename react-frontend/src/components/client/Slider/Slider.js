776255d85275a340c26390e7f3c58cb29ff77b0e
KirKhokhlov <khokhlov785@mail.ru>
2025-01-06 00:34:32 +0300

    slider res fix

--- react-frontend/src/components/client/Slider/Slider.js
+++ react-frontend/src/components/client/Slider/Slider.js
@@ -2,12 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
 import './Slider.css';
-
-import arrowLeft from '../../../resources/Slider/Images/arrow_left.png';
-import arrowRight from '../../../resources/Slider/Images/arrow_right.png';
-
 
 function SampleNextArrow(props) {
     const { onClick } = props;
@@ -16,20 +11,14 @@ function SampleNextArrow(props) {
         style={{
 
             display: "block",
-
             position: "absolute",
-
             top: "50%",
-
-            right: "-100px", // Позиция по правому краю
-
-            transform: "translateY(-50%)", // Центрирование по вертикали
-
-            zIndex: 1 // Чтобы стрелка была поверх других элементов
-
+            right: "-100px", 
+            transform: "translateY(-50%)", 
+            zIndex: 1 
         }}
             onClick={onClick}>
-            <img src={arrowRight} alt=''></img>
+            <img src={'/Slider/Images/arrow_right.png'} alt=''></img>
         </div>
     );
 }
@@ -41,53 +30,21 @@ function SamplePrevArrow(props) {
         style={{
 
             display: "block",
-
             position: "absolute",
-
             top: "50%",
-
-            left: "-100px", // Позиция по левому краю
-
-            transform: "translateY(-50%)", // Центрирование по вертикали
-
-            zIndex: 1 // Чтобы стрелка была поверх других элементов
-
+            left: "-100px", 
+            transform: "translateY(-50%)", 
+            zIndex: 1 
         }}
             onClick={onClick}>
-            <img src={arrowLeft} alt=''></img>
+            <img src={'/Slider/Images/arrow_left.png'} alt=''></img>
         </div>
     );
 }
 
 export default function SimpleSlider(props) {
-    // const [defaultImage, setDefaultImage] = useState({});
-    // const settings = {
-    //     // className: "center",
-    //     // centerMode: true,
-    //     // infinite: true,
-    //     centerPadding: "0px",
-    //     slidesToShow: 4,
-    //     speed: 500,
-    //     nextArrow: <SampleNextArrow />,
-    //     prevArrow: <SamplePrevArrow />,
-    //     responsive: [
-    //         {
-    //             breakpoint: 1890,
-    //             settings: {
-    //                 slidesToShow: 3,
-    //             },
-    //         },
-    //         {
-    //             breakpoint: 1430,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //             },
-    //         },
-    //     ],
-    // };
     const sett = props.settings
     const className = `Slider ${props.main ? "": 'Centered-slider'}`
-
     return (
         <div className={className}>
             {props.main ? <div style={{ textAlign: 'center' }}>
